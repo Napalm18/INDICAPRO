3b6504e9b58be4bbfa5f78252c8ff4b0283c521f
Napalm18 <gabrielsoaresdacosta18@gmail.com>
2025-09-03 09:57:36 -0300

    Fix import path for errorHandler with .jsx extension

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import Configuracoes from './componets/pages/Configuracoes';
 import { Toaster } from './componets/ui/toaster';
 import { NotificationProvider } from './componets/contexts/NotificationContext';
 import { ThemeProvider } from './componets/contexts/ThemeContext';
-import { AIErrorBoundary } from './lib/errorHandler.js';
+import { AIErrorBoundary } from './lib/errorHandler.jsx';
 
 function AppRoutes() {
   const { user, loading } = useAuth();
